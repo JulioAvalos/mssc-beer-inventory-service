d974f8107987d2b6cefc642ba7e8ccf2caa0c1aa
JulioAvalos <julioavalos2@msn.com>
2020-05-27 01:43:25 -0600

    Adding a new inventory listener to listen the jms queue

---
+++ src/main/java/guru/sfg/beer/inventory/service/service/NewInventoryListener.java
@@ -0,0 +1,39 @@
+package guru.sfg.beer.inventory.service.service;
+
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
+import guru.sfg.common.events.BeerDto;
+import guru.sfg.common.events.NewInventoryEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class NewInventoryListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void listen(NewInventoryEvent event){
+
+        log.debug("Got Inventory: " + event.toString());
+
+        List<BeerInventory> list = beerInventoryRepository.findAll();
+
+        log.debug("for debuggin purposes! -> " + list.toString());
+
+        beerInventoryRepository.save(BeerInventory.builder()
+                .beerId(event.getBeerDto().getId())
+                .upc(event.getBeerDto().getUpc())
+                .quantityOnHand(event.getBeerDto().getQuantityOnHand())
+                .build());
+    }
+
+}
