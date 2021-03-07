df51ae5e34dc8b5563a2e0e43e99dd29051e4a44
Julio Avalos <julioavalos2@msn.com>
2021-03-06 23:01:00 -0600

    Adding Allocation Service - closes #11

---
+++ src/main/java/guru/sfg/beer/inventory/service/service/AllocationService.java
@@ -0,0 +1,8 @@
+package guru.sfg.beer.inventory.service.service;
+
+import guru.sfg.brewery.model.BeerOrderDto;
+
+public interface AllocationService {
+
+    Boolean allocateOrder(BeerOrderDto beerOrderDto);
+}
